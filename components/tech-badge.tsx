c9f2cf4ab5133220454951d2e9972b833f866ca7
Ojasvi <ojasvidoye@gmail.com>
2025-05-26 15:00:26 +0530

    update

--- components/tech-badge.tsx
+++ components/tech-badge.tsx
@@ -4,7 +4,7 @@ interface TechBadgeProps {
 
 export function TechBadge({ tech }: TechBadgeProps) {
   return (
-    <span className="inline-flex items-center px-1 py-1/2 text-xs font-medium bg-zinc-700 border border-zinc-600/90  text-zinc-300 rounded-lg">
+    <span className="inline-flex items-center px-2 py-[1/2] text-xs font-semibold bg-zinc-300/90 text-zinc-900 border-none rounded-lg">
       {tech}
     </span>
   );
